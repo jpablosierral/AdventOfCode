b96b1cbaf99c24fb1fa156b7e8d431320fe8c1d4
Pablo Sierra <jpablosierral@gmail.com>
2018-12-07 17:39:33 +0100

    Day 3 Part 2

--- Day3/Day3-Part2.js
+++ Day3/Day3-Part2.js
@@ -2,11 +2,13 @@ const input = document.body.getElementsByTagName('pre')[0].innerHTML
     .split(/\n/g)
     .filter(value => value !== '');
 
-let result = 0;
+let candidate = '';
 let fabric = Array(1).fill().map(() => Array(0).fill());
+let candidates = [];
 
 input.forEach(element => {
     const aux = element.split(' ');
+    const id = aux[0].split('#')[1];
     const position = aux[2].split(':')[0].split(',');
     const size = aux[3].split('x');
     const left = parseInt(position[0]);
@@ -14,20 +16,32 @@ input.forEach(element => {
     const wide = parseInt(size[0]);
     const tall = parseInt(size[1]);
 
+    candidates.push(id);
+    
     for (let i = 0; i < wide; i++) {
         for (let j = 0; j < tall; j++) {
             if (fabric[left + i] === undefined){
                 fabric[left + i] = [];
             }
             if (fabric[left + i][top +j] === undefined){
-                fabric[left + i][top +j] = 0;
+                fabric[left + i][top +j] = [];
             }
-            fabric[left + i][top +j] += 1;
-            if(fabric[left + i][top +j] === 2){
-                result += 1;
-            }
+            fabric[left + i][top +j].push(id);
         }
     }
 });
 
-console.info('Result: ' + result);
+for (let i = 0; i < fabric.length; i++) {
+    for (let j = 0; j < fabric[i].length; j++) {
+        if (fabric[i][j] !== undefined && fabric[i][j].length > 1){
+            for (let k = 0; k < fabric[i][j].length; k++) {
+                const index = candidates.indexOf(fabric[i][j][k]);
+                if (index > -1){
+                    candidates.splice(index, 1);
+                }
+            }
+        }
+    }
+}
+
+console.info('Result: ' + candidates[0] + ' (of ' + candidates.length + ')');
